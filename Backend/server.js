da1512b97d80e645a3b8bd7644b092402f664445
Gauri Nagariya <gaurinagariya@gmail.com>
2026-05-16 19:01:54 +0530

    url changed

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",                 // development
   // "https://meditrack-black.vercel.app",
-  "https://medi-track-roan.vercel.app",
+  "https://meditrackproject.vercel.app",
   "https://meditrack-1-bhm8.onrender.com"     // production
 ];
 
